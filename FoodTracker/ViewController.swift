cb610e70ed2b54b1828029d45a7ed477289980f7
Tyler <tylercweddle@gmail.com>
2018-01-22 19:27:49 -0500

    Part 2 of tutorial

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Delegate callbacks for the text field
+        nameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    //Once user hits 'return' in keyboard, the keyboard should disappear
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 }
 
